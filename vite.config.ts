3304b5a9d15a6ffda0afcf12f26390dbc1ee6dca
slyim <Paragosa@protonmail.com>
2026-05-13 13:03:48 +0300

    fix: revert base path to / for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/zelva-portfolio/',
+  base: '/',
   plugins: [
     vue(),
     mode === 'development' ? vueDevTools() : undefined,
